8d91d3343bc570597543ac524822e22a3b4ad21e
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-03-03 18:26:38 +0000

    We create a new test for our new VotingPollFactory which again gets our foot through the door. We throw an exception if not enough counter names are supplied.

--- VotingSystem.Tests/VotingPollTests.cs
+++ VotingSystem.Tests/VotingPollTests.cs
@@ -14,11 +14,32 @@ namespace VotingSystem.Tests
         public void ZeroCountersWhenCreated() 
         {
             var poll = new VotingPoll();
+
             Empty(poll.Counters);
         }
     }
     public class VotingPollFactoryTests
     {
+        [Fact]
+        public void Create_ThrowIfLessThanTwoCounterNames() 
+        {
+            var names = new[] { "name"};
+            var factory = new VotingPollFactory();
+
+            Throws<ArgumentException>(() => factory.Create(names));
+        }
+    }
+
+    public class VotingPollFactory
+    {
+        public VotingPollFactory()
+        {
+        }
+
+        internal void Create(string[] names)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class VotingPoll
