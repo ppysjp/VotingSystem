a2a0f94ba37baf1ce6f9c4bd5607b576d6f2082a
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-03-03 17:55:15 +0000

    We create a test that again gets our foot through the door.

--- VotingSystem.Tests/VotingPollTests.cs
+++ VotingSystem.Tests/VotingPollTests.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
 namespace VotingSystem.Tests
 {
-    class VotingPollTests
+    public class VotingPollTests
     {
+        [Fact]
+        public void ZeroCountersWhenCreated() 
+        {
+            var poll = new VotingPoll();
+            Assert.Empty(poll.Counters);
+        }
+    }
+
+    public class VotingPoll
+    {
+        public VotingPoll()
+        {
+        }
+
+        public IEnumerable Counters { get; internal set; }
     }
 }
