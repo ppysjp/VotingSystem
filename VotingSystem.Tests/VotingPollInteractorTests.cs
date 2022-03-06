b2d935adb73eeab69f6c6fff367153e94e685492
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-03-06 20:57:18 +0000

    The first thing we want to check is that the Interactor will in fact create a VotingPoll using the VotingPollFactory. The primary goal of the voting poll interactor is to create a voting poll and then save it to a database.

--- VotingSystem.Tests/VotingPollInteractorTests.cs
+++ VotingSystem.Tests/VotingPollInteractorTests.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using  Xunit;
+using static Xunit.Assert;
 
 namespace VotingSystem.Tests
 {
-    class VotingPollInteractorTests
+    public class VotingPollInteractorTests
     {
+        [Fact]
+        public void CreateVotingPoll_UsesVotingPollFactoryToCreateVotingPoll()
+        {
+            var request = new VotingPollFactory.Request();
+            var mockLibrary = new Mock<VotingPollFactory>();
+            var factory = new VotingPollFactory();
+            var interactor = new VotingPollInteractor(mockLibrary.Object);
+
+            interactor.CreateVotingPoll(request);
+
+            mockLibrary.Verify(x => x.Create(request));
+        }
+    }
+
+    public class VotingPollInteractor
+    {
+        private readonly VotingPollFactory _factory;
+
+        public VotingPollInteractor(VotingPollFactory factory)
+        {
+            _factory = factory;
+        }
+
+        internal void CreateVotingPoll(VotingPollFactory.Request request)
+        {
+            _factory.Create(request);
+        }
     }
 }
