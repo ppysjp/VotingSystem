3895d4e7dbd23274255692e10530e6585950f716
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-03-16 17:09:51 +0000

    You can use Entity framework to produce queries to your database. a DbSet is a collection because it implements the IEnumerable interface, that is the key thing you need to know at this stage.

--- Sandbox.EFCore/Program.cs
+++ Sandbox.EFCore/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
-namespace Sandbox.EFCore
+namespace Sandbox.EfCore
 {
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Fruit> Fruits { get; set; }
+    }
+    
+    public class Fruit 
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! EfCore");
+            IEnumerable<Fruit> fruits = new[] { new Fruit { Name = "Banana", Weight = 1 } }; 
         }
     }
 }
