6ba0ef17d353fa850d4ccb04f4a8a4071cd29abf
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-12 17:54:57 +0100

    Create first test using tdd

--- VotingSystem.Database.Tests/DatabaseTests.cs
+++ VotingSystem.Database.Tests/DatabaseTests.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
 
 namespace VotingSystem.Database.Tests
 {
     public class DatabaseTests
     {
+        [Fact]
+        public void SavesCounterToDatabase() 
+        {
+            using (var ctx = new AppDbContext()) 
+            { 
+                ctx.Counters
+            }
+
+            using (var ctx = new AppDbContext()) 
+            { 
+
+            }
+        }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public DbSet<Counter> Counters { get; set; }
     }
 }
