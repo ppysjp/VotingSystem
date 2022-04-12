deb99cad0fc9b73fd240ac0b030178191b8d5618
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-12 21:18:11 +0100

    Begin creating foot in the door test for the voting poll

--- VotingSystem.Database.Tests/DatabaseTests.cs
+++ VotingSystem.Database.Tests/DatabaseTests.cs
@@ -30,28 +30,6 @@ namespace VotingSystem.Database.Tests
             }
         }
 
-        [Fact]
-        public void SavesCounterToDatabase2() 
-        {
-            var options = new DbContextOptionsBuilder<AppDbContext>()
-                .UseInMemoryDatabase(nameof(SavesCounterToDatabase))
-                .Options;
-
-            var counter = new Counter { Name = "New Counter"};
-
-            using (var ctx = new AppDbContext(options)) 
-            {
-                ctx.Counters.Add(counter);
-                ctx.Counters.Add(counter);
-                ctx.SaveChanges();
-            }
-
-            using (var ctx = new AppDbContext(options)) 
-            {
-                var savedCounter = ctx.Counters.ToList();
-            }
-        }
-
 
     }
 
