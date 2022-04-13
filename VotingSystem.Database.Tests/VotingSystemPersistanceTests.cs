804b0f220d8ed1a00f35657b50d04ae943adfd66
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-13 19:25:53 +0100

    Began first test and createed classs that implements IVotingSystemPersistance

--- VotingSystem.Database.Tests/VotingSystemPersistanceTests.cs
+++ VotingSystem.Database.Tests/VotingSystemPersistanceTests.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VotingSystem.Application;
+using VotingSystem.Models;
+using Xunit;
 
 namespace VotingSystem.Database.Tests
 {
     public class VotingSystemPersistanceTests
     {
-        
+       [Fact] 
+       public void SavesVotingPollToDatabase() 
+        {
+            IVotingSystemPersistance persistance = new VotingSystemPersistance();
+        }
+    }
+
+    public class VotingSystemPersistance : IVotingSystemPersistance
+    {
+        public void SaveVotingPoll(VotingPoll votingPoll)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
