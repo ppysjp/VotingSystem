ea2fa085e86da7b9a87e85525a2a50fadf496a25
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-20 21:15:45 +0100

    Create Vote and VotingInteractor objects

--- VotingSystem.Application.Tests/VotingInteractorTests.cs
+++ VotingSystem.Application.Tests/VotingInteractorTests.cs
@@ -4,15 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
 
 namespace VotingSystem.Application.Tests
 {
     public class VotingInteractorTests
     {
+        private Mock<IVotingSystemPersistance> _mockPersistance = new Mock<IVotingSystemPersistance>();
+        
         [Fact]
         public void Vote_PersistsVote() 
-        { 
+        {
+            var vote = new Vote();
+            var interactor = new VotingInteractor(_mockPersistance.Object);
         }
         
+    }
+
+    public class Vote
+    {
+        public Vote()
+        {
+        }
+    }
+
+    public class VotingInteractor
+    {
+        private IVotingSystemPersistance @object;
+
+        public VotingInteractor(IVotingSystemPersistance @object)
+        {
+            this.@object = @object;
+        }
     }
 }
