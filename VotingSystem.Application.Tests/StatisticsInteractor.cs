627bba09693b5a736210400ea45007c2e3b5c48e
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-21 15:52:05 +0100

    We know that when we display the statistics we are going to make use of a Voting Poll

--- VotingSystem.Application.Tests/StatisticsInteractor.cs
+++ VotingSystem.Application.Tests/StatisticsInteractor.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VotingSystem.Models;
+using Xunit;
 
 namespace VotingSystem.Application.Tests
 {
     public class StatisticsInteractor
     {
-        
+       [Fact] 
+       public void DisplayPollStatics() 
+       { 
+            
+            var poll = new VotingPoll
+            {
+                Title = "title",
+                Description = "desc",
+                Counters = new List<Counter>
+                {
+                    new Counter {Name = "One" },
+                    new Counter {Name = "Two" },
+                }
+            };
+
+       }
+
     }
 }
