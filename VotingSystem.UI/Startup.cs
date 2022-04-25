d4067a6e5ea702d8d463106b1984ce301d4f31da
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-25 21:23:55 +0100

    Learning about how the Configure function works within the Startup.cs

--- VotingSystem.UI/Startup.cs
+++ VotingSystem.UI/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -49,7 +50,12 @@ namespace VotingSystem.UI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World");
+                });
+
+                //endpoints.MapRazorPages();
             });
         }
     }
