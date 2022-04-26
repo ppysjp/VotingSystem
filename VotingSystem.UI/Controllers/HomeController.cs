9511aae7eb691343d61fc3242744daf354cfd502
Samuel Palmer <Samuel.Palmer@dpr.co.uk>
2022-04-26 16:13:11 +0100

    Create HomeController

---
+++ VotingSystem.UI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotingSystem.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
